de461d0c10dd5373a259d2b5b2b748471337c8ef
vieros <96722902+vieros@users.noreply.github.com>
2025-09-07 03:31:40 +0300

    Update vieros.js

--- vieros.js
+++ vieros.js
@@ -1,1 +1,124 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { distubeOptions } = require('./src/config/config.js');
+const fs = require('fs');
+const path = require('path');
+const PlayerManager = require('./src/player/PlayerManager.js');
+const { printWatermark } = require('./src/config/type.js');
+const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.GuildMessages
+];
 
+const client = new Client({ intents });
+printWatermark();
+client.commands = new Collection();
+client.playerManager = new PlayerManager(client, distubeOptions);
+client.playerManager.distube.setMaxListeners(20);
+const commandsPath = path.join(__dirname, './src/commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  client.commands.set(command.data.name, command);
+}
+
+
+const eventsPath = path.join(__dirname, './src/events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(""); ///// token dane by vieros devloper
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { distubeOptions } = require('./src/config/config.js');
+const fs = require('fs');
+const path = require('path');
+const PlayerManager = require('./src/player/PlayerManager.js');
+const { printWatermark } = require('./src/config/type.js');
+const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.GuildMessages
+];
+
+const client = new Client({ intents });
+printWatermark();
+client.commands = new Collection();
+client.playerManager = new PlayerManager(client, distubeOptions);
+client.playerManager.distube.setMaxListeners(20);
+const commandsPath = path.join(__dirname, './src/commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  client.commands.set(command.data.name, command);
+}
+
+
+const eventsPath = path.join(__dirname, './src/events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+///// token dane by vieros dev
+client.login("");
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { distubeOptions } = require('./src/config/config.js');
+const fs = require('fs');
+const path = require('path');
+const PlayerManager = require('./src/player/PlayerManager.js');
+const { printWatermark } = require('./src/config/type.js');
+const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.GuildMessages
+];
+
+const client = new Client({ intents });
+printWatermark();
+client.commands = new Collection();
+client.playerManager = new PlayerManager(client, distubeOptions);
+client.playerManager.distube.setMaxListeners(20);
+const commandsPath = path.join(__dirname, './src/commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  client.commands.set(command.data.name, command);
+}
+
+
+const eventsPath = path.join(__dirname, './src/events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+////// by dev vieros
+client.login(""); //// token acc dane
